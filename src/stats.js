4899df39052b09a560f8ab5d5605d4157bcd7615
Wolfram Kriesing <wolfram+github@kriesing.de>
2020-04-02 17:37:38 +0200

    Extract reducer as own function.

--- src/stats.js
+++ src/stats.js
@@ -7,7 +7,7 @@ const countSuites = (suites) => {
    * @param {number} count
    * @param {import("./stats").Suite} suite
    * @returns {number}
-   */  
+   */
   const reducer = (count, suite) => {
     const childSuites = suite.suites ? suite.suites : [];
     return count + countSuites(childSuites);
@@ -19,18 +19,18 @@ const countSuites = (suites) => {
  * @param {import("./stats").Suite[]} suites
  * @returns {number}
  */
-const countTestsInSuites = (suites) =>
-  suites.reduce(
-      /**
-       * @param {number} count
-       * @param {import("./stats").Suite} suite
-       * @returns {number}
-       */  
-    (count, suite) => {
-      const countInChildSuites = countTestsInSuites(suite.suites);
-      return count + suite.tests.length + countInChildSuites;
-    }, 0
-  );
+const countTestsInSuites = (suites) => {
+  /**
+   * @param {number} count
+   * @param {import("./stats").Suite} suite
+   * @returns {number}
+   */
+  const reducer = (count, suite) => {
+    const countInChildSuites = countTestsInSuites(suite.suites);
+    return count + suite.tests.length + countInChildSuites;
+  }
+  return suites.reduce(reducer, 0);
+}
 /**
  * @param {import("./stats").Suite} all
  * @returns {number}
