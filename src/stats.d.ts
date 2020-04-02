8aa5727e11289f1e83a57ecf373a3701199edcd9
Wolfram Kriesing <wolfram+github@kriesing.de>
2020-04-02 17:26:03 +0200

    Typing the stats. Walk in the park ... not so easy these times :/.

---
+++ src/stats.d.ts
@@ -0,0 +1,14 @@
+type Test = {};
+
+export type Suite = {
+  name: string;
+  suites: Suite[];
+  tests: Test[];
+}
+
+export type Stats = {
+  counts: {
+    tests: number;
+    suites: number;
+  }
+}
--- src/stats.js
+++ src/stats.js
@@ -1,4 +1,13 @@
+/**
+ * @param {import("./stats").Suite[]} suites
+ * @returns {number}
+ */
 const countSuites = (suites) => {
+  /**
+   * @param {number} count
+   * @param {import("./stats").Suite} suite
+   * @returns {number}
+   */  
   const reducer = (count, suite) => {
     const childSuites = suite.suites ? suite.suites : [];
     return count + countSuites(childSuites);
@@ -6,14 +15,31 @@ const countSuites = (suites) => {
   const childrenCount = suites.reduce(reducer, 0);
   return childrenCount + suites.length;
 };
+/**
+ * @param {import("./stats").Suite[]} suites
+ * @returns {number}
+ */
 const countTestsInSuites = (suites) =>
   suites.reduce(
+      /**
+       * @param {number} count
+       * @param {import("./stats").Suite} suite
+       * @returns {number}
+       */  
     (count, suite) => {
       const countInChildSuites = countTestsInSuites(suite.suites);
       return count + suite.tests.length + countInChildSuites;
     }, 0
   );
+/**
+ * @param {import("./stats").Suite} all
+ * @returns {number}
+ */
 const countTests = (all) => countTestsInSuites(all.suites) + all.tests.length;
+/**
+ * @param {import("./stats").Suite} all
+ * @returns {import("./stats").Stats}
+ */
 export const stats = (all) => {
   const {suites} = all;
   const testsCount = countTests(all);
