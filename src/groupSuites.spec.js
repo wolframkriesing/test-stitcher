52b0994407fd4a7c2ae2e2ecd1bd2502e73bc871
Wolfram Kriesing <wolfram+github@kriesing.de>
2020-04-03 18:08:44 +0200

    Build a root-suite when grouping even one suite, this became obvious when I wanted to group two suites.

--- src/groupSuites.spec.js
+++ src/groupSuites.spec.js
@@ -19,30 +19,37 @@ const groupSuites = (suites) => {
   if (!suite.name) {
     suite.name = suite.origin;
   }
-  return suite;
+  return {name: 'root', suites: [suite], tests: [], origin: ''};
 };
 
 describe('Group test suites from multiple files and produce one containing them all', () => {
   describe('GIVEN every file has a relative path (e.g. src/file1 and src/file2)', () => {
     describe('WHEN given one file AND the suite has NO name ', () => {
       const suite = {name: '', suites: [], tests: [], origin: 'file.js'};
-      it('THEN return this one suite with filename as suite name', () => {
+      it('THEN return this one suite as child-suite, with the name "root"', () => {
         const groupedSuite = groupSuites([suite]);
-        assert.strictEqual(groupedSuite.name, suite.origin);
-        assert.deepStrictEqual(groupedSuite.suites, suite.suites);
-        assert.deepStrictEqual(groupedSuite.tests, suite.tests);
-        assert.strictEqual(groupedSuite.origin, suite.origin);
+        assert.strictEqual(groupedSuite.name, 'root');
+        assert.strictEqual(groupedSuite.suites.length, 1);
       });
-      it('THEN the suite must be cloned', () => {
+      it('AND return the grouped suite with filename as suite name', () => {
         const groupedSuite = groupSuites([suite]);
-        assert.notStrictEqual(groupedSuite, suite);
+        const childSuite = groupedSuite.suites[0];
+        assert.strictEqual(childSuite.name, suite.origin);
+        assert.deepStrictEqual(childSuite.suites, suite.suites);
+        assert.deepStrictEqual(childSuite.tests, suite.tests);
+        assert.strictEqual(childSuite.origin, suite.origin);
+      });
+      it('AND the suite must be cloned', () => {
+        const groupedSuite = groupSuites([suite]);
+        assert.notStrictEqual(groupedSuite.suites[0], suite);
       });
     });
     describe('WHEN given one file AND the suite has a name ', () => {
       const suite = {name: 'Existing name', suites: [], tests: [], origin: 'file.js'};
-      it('THEN return this one suite with original suite name', () => {
+      it('THEN return this one suite as child-suite with original suite name', () => {
         const groupedSuite = groupSuites([suite]);
-        assert.deepStrictEqual(groupedSuite, suite);
+        const childSuite = groupedSuite.suites[0];
+        assert.deepStrictEqual(childSuite, suite);
       });
     });
   });
