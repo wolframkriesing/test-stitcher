76210ceaca406f1a2a08478d28ddcde1bd9fc384
Wolfram Kriesing <wolfram+github@kriesing.de>
2020-04-03 18:25:06 +0200

    We do always give the suite the name of the file. Decided.

--- src/groupSuites.spec.js
+++ src/groupSuites.spec.js
@@ -11,20 +11,31 @@ const cloneSuite = (suite) => {
 };
 
 /**
+ * @param {Suite} suite
+ * @returns {Suite}
+ */
+const cloneSuiteAndNameIt = (suite) => {
+  const clone = cloneSuite(suite);
+  clone.name = suite.origin;
+  return clone;
+}
+
+/**
  * @param {Suite[]} suites
  * @returns {Suite}
  */
 const groupSuites = (suites) => {
-  const suite = cloneSuite(suites[0]);
-  if (!suite.name) {
-    suite.name = suite.origin;
-  }
-  return {name: 'root', suites: [suite], tests: [], origin: ''};
+  return {
+    name: 'root', 
+    suites: suites.map(cloneSuiteAndNameIt), 
+    tests: [], 
+    origin: ''
+  };
 };
 
 describe('Group test suites from multiple files and produce one containing them all', () => {
   describe('GIVEN every file has a relative path (e.g. src/file1 and src/file2)', () => {
-    describe('WHEN given one suite of one file AND the suite has NO name ', () => {
+    describe('WHEN given one suite of one file', () => {
       const suite = {name: '', suites: [], tests: [], origin: 'file.js'};
       it('THEN return this one suite as child-suite, with the name "root"', () => {
         const groupedSuite = groupSuites([suite]);
@@ -44,20 +55,27 @@ describe('Group test suites from multiple files and produce one containing them 
         assert.notStrictEqual(groupedSuite.suites[0], suite);
       });
     });
-    describe('WHEN given one file AND the suite has a name ', () => {
-      const suite = {name: 'Existing name', suites: [], tests: [], origin: 'file.js'};
-      it('THEN return this one suite as child-suite with original suite name', () => {
-        const groupedSuite = groupSuites([suite]);
-        const childSuite = groupedSuite.suites[0];
-        assert.deepStrictEqual(childSuite, suite);
-      });
-    });
-    describe('WHEN given suites of two files AND the suites have no name ', () => {
+    describe('WHEN given suites of two files', () => {
       const suite1 = {name: '', suites: [], tests: [], origin: 'file1.js'};
       const suite2 = {name: '', suites: [], tests: [], origin: 'file2.js'};
-      it('THEN return this one suite with original suite name', () => {
-        // const groupedSuite = groupSuites([suite]);
-        // assert.deepStrictEqual(groupedSuite, suite);
+      it('THEN return the suites with filename as suite name', () => {
+        const groupedSuite = groupSuites([suite1, suite2]);
+        assert.deepStrictEqual(groupedSuite.suites[0].name, suite1.origin);
+        assert.deepStrictEqual(groupedSuite.suites[1].name, suite2.origin);
+      });
+    });
+    xdescribe('WHEN multiple suites are in one sub-directory', () => {
+      const suite1 = {name: '', suites: [], tests: [], origin: 'dir1/file1.js'};
+      const suite2 = {name: '', suites: [], tests: [], origin: 'dir1/file2.js'};
+      it('THEN create a child-suite named like the directory', () => {
+        const groupedSuite = groupSuites([suite1, suite2]);
+        assert.deepStrictEqual(groupedSuite.suites[0].name, 'dir1');
+      });
+      it('AND the suites underneath', () => {
+        const groupedSuite = groupSuites([suite1, suite2]);
+        const childSuite = groupedSuite.suites[0];
+        assert.deepStrictEqual(childSuite.suites[0].name, 'file1.js');
+        assert.deepStrictEqual(childSuite.suites[1].name, 'file2.js');
       });
     });
   });
